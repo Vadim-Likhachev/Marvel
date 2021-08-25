d20cc7f54469a47d02ffdb99c062db5690975cdb
Vadim Likhachev <vadlen2149@gmail.comm>
2021-08-25 18:21:48 +0300

    marvel-heroes

---
+++ marvel/src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Marvel</a>
+          <form class="d-flex">
+            <input class="form-control me-2"
+             type="search" 
+             placeholder="Поиск..." 
+             aria-label="Search"
+             v-model="search"
+             @input="changeSearch(search)">
+            <button class="btn btn-outline-light" type="submit">Найти</button>
+          </form>
+        </div>
+      </nav>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader",
+        props: ['changeSearch'],
+        data() {
+            return {
+              search: '',
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
